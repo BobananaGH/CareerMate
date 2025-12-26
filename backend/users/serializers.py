e8b7806a46b3aba1330bbe817544f121b4a5de4d
Bob <Bobanana0932@gmail.com>
2025-12-27 03:41:02 +0700

    Login API working with email authentication

---
+++ backend/users/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        try:
+            user_obj = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Invalid email or password")
+
+        user = authenticate(
+            username=user_obj.username,  # 🔑 THIS is the key
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+
+        attrs["user"] = user
+        return attrs
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
 
-# Create your views here.
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+
+        return Response({
+            "message": "Login successful",
+            "user": {
+                "id": user.id,
+                "email": user.email,
+            }
+        }, status=status.HTTP_200_OK)
