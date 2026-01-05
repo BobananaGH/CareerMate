9ec9661fe9c4aa7ac9b1f55dc3a75b37cdd3bdab
Bob <Bobanana0932@gmail.com>
2026-01-05 18:07:15 +0700

    Added Google auth + token refresh

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import LoginAPIView, ProtectedView, MeView
+from .views import LoginAPIView, ProtectedView, MeView, GoogleLoginAPIView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
+    path("google-login/", GoogleLoginAPIView.as_view(), name="google-login"),
     path("protected/", ProtectedView.as_view(), name="protected"),  
     path("me/", MeView.as_view(), name="me"),
     
+]
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns += [
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
