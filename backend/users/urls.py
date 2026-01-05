ff87850fa97aead4e521595e2f0a93d235d7713e
Bob <Bobanana0932@gmail.com>
2026-01-05 21:18:10 +0700

    Added Remember Me? deleted old HTMLs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
-from .views import LoginAPIView, ProtectedView, MeView, GoogleLoginAPIView
+from .views import (
+    LoginAPIView,
+    ProtectedView,
+    MeView,
+    GoogleLoginAPIView,
+    PasswordResetRequestAPIView,
+    PasswordResetConfirmAPIView,
+    PasswordResetCompleteAPIView,
+)
 from rest_framework_simplejwt.views import TokenRefreshView
 
+# users/urls.py
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("google-login/", GoogleLoginAPIView.as_view(), name="google-login"),
-    path("protected/", ProtectedView.as_view(), name="protected"),  
+    path("protected/", ProtectedView.as_view(), name="protected"),
     path("me/", MeView.as_view(), name="me"),
-    
-]
-from rest_framework_simplejwt.views import TokenRefreshView
 
-urlpatterns += [
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("password-reset/", PasswordResetRequestAPIView.as_view()),
+    path("password-reset/confirm/", PasswordResetConfirmAPIView.as_view()),  # <--- just this
+    path("password-reset/complete/", PasswordResetCompleteAPIView.as_view()),
 ]
