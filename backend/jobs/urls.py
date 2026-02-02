75bf4855a626b3f41076b9efebfcf10230b2886a
Bob <Bobanana0932@gmail.com>
2026-02-03 04:24:20 +0700

    Job Implementation

---
+++ backend/jobs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ApplicationStatusUpdateView, JobListCreateView, ApplyJobView, RecruiterApplicationsView, MyApplicationsView
+
+urlpatterns = [
+    path("", JobListCreateView.as_view()),
+    path("apply/", ApplyJobView.as_view()),
+    path("applications/", RecruiterApplicationsView.as_view()),
+    path("applications/<int:pk>/", ApplicationStatusUpdateView.as_view()), 
+    path("my-applications/", MyApplicationsView.as_view()),
+]
