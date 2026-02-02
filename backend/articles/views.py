4f744cbbe244376448b338b8d058b8b12995a912
Bob <Bobanana0932@gmail.com>
2026-02-02 20:55:36 +0700

    Article Detail Implementation

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from django.shortcuts import get_object_or_404
+
 from .models import Article
 from .serializers import ArticleSerializer
 
@@ -20,4 +23,12 @@ class ArticleCreateAPIView(APIView):
         serializer = ArticleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)
+
+class ArticleDetailAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article)
         return Response(serializer.data)
