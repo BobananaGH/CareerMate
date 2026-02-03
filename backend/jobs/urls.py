c8f30dc79342425d9e353079a24e4b5623bc6db3
Bob <Bobanana0932@gmail.com>
2026-02-03 07:03:12 +0700

    Debugging

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -3,10 +3,12 @@ from .views import ApplicationStatusUpdateView, JobListCreateView, ApplyJobView,
 
 urlpatterns = [
     path("", JobListCreateView.as_view()),
-    path("<int:pk>/", JobDetailView.as_view()),     
+
     path("apply/", ApplyJobView.as_view()),
     path("applications/", RecruiterApplicationsView.as_view()),
     path("applications/<int:pk>/", ApplicationStatusUpdateView.as_view()),
     path("my-applications/", MyApplicationsView.as_view()),
+
+    path("<int:pk>/", JobDetailView.as_view()),
 ]
 
