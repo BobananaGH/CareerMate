59f80010d8edd30b554e153e75d3472ff901c6dd
Bob <Bobanana0932@gmail.com>
2026-02-05 00:56:07 +0700

    Fixed Card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,8 +1,8 @@
 import styles from "./css/Card.module.css";
 
-export default function Card({ title, children, footer, actions }) {
+export default function Card({ title, children, footer, actions, onClick }) {
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={onClick}>
       <h2>{title}</h2>
 
       <div className={styles.body}>{children}</div>
