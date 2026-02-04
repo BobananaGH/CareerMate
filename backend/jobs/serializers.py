22d47ec7af59f719853964e096f4122f12c72f00
Bob <Bobanana0932@gmail.com>
2026-02-04 20:47:24 +0700

    Job Serializer Update

--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -32,19 +32,31 @@ class JobSerializer(serializers.ModelSerializer):
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    job = serializers.PrimaryKeyRelatedField(
+        queryset=Job.objects.all(),
+        write_only=True
+    )
+
     candidate_email = serializers.EmailField(source="candidate.email", read_only=True)
+    job_title = serializers.CharField(source="job.title", read_only=True)
+    job_location = serializers.CharField(source="job.location", read_only=True)
+    job_id = serializers.IntegerField(source="job.id", read_only=True)
 
     class Meta:
         model = Application
         fields = [
             "id",
-            "job",
+            "job",          
+            "job_id",     
+            "job_title",
+            "job_location",
             "candidate_email",
-            "cv",
+            "cv",      
             "status",
             "created_at",
         ]
         read_only_fields = ["created_at", "candidate"]
+
         extra_kwargs = {
             "cv": {"required": False, "allow_null": True},
         }
