3a10db0748fb1963085f4000f73f6c856786ab33
Bob <Bobanana0932@gmail.com>
2026-02-05 01:00:21 +0700

    Wired Urls

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ApplicationStatusUpdateView, JobListCreateView, ApplyJobView, RecruiterApplicationsView, MyApplicationsView, JobDetailView
+from .views import ApplicationStatusUpdateView, JobListCreateView, ApplyJobView, RecruiterApplicationsView, MyApplicationsView, JobDetailView, UnapplyJobView
 
 urlpatterns = [
     path("", JobListCreateView.as_view()),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("applications/", RecruiterApplicationsView.as_view()),
     path("applications/<int:pk>/", ApplicationStatusUpdateView.as_view()),
     path("my-applications/", MyApplicationsView.as_view()),
+    path("unapply/<int:job_id>/", UnapplyJobView.as_view()),
 
     path("<int:pk>/", JobDetailView.as_view()),
 ]
